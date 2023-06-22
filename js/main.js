52a4e39e934f3055e4e0474c59441bfaffbba96b
TuanKha0904 <kha090403@gmail.com>
2023-06-22 11:53:54 +0700

    change background login register

--- js/main.js
+++ js/main.js
@@ -174,17 +174,3 @@ Created: Colorib
 })(jQuery);
 
 
-(function($) {
-
-    $(".toggle-password").click(function() {
-
-        $(this).toggleClass("zmdi-eye zmdi-eye-off");
-        var input = $($(this).attr("toggle"));
-        if (input.attr("type") == "password") {
-          input.attr("type", "text");
-        } else {
-          input.attr("type", "password");
-        }
-      });
-
-})(jQuery);
